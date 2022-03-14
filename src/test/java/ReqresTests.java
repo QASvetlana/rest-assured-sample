793d25958ff6f66bf13d60cab43e4947424d9817
Svetlana <89111378147@yandex.ru>
2022-03-14 17:53:19 +0300

    test for reqres added

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,31 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @Test
+    void createUser() {
+        String data = "{\"name\": \"morpheus\",\"job\": \"leader\"}";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name",is("morpheus"));
+
+    }
+
+}
